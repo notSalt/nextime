541448de45e6193ba0330a2efad845102ec4b0ef
_notSalt <joshuakoh303@gmail.com>
2025-07-17 07:05:02 +0800

    same site none session

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,9 +31,9 @@ app.use(
 		cookie: {
 			secure: true,
 			httpOnly: false,
-			sameSite: 'lax',
+			sameSite: 'none',
 			maxAge: 1000 * 60 * 60 * 24 * 7,
-			domain: 'nextime.notsalt.com'
+			domain: '.notsalt.com'
 		}, // 7 days
 	})
 );
